6a39c10f463b935f576349700589b86eb277de3e
tambat <tambat.sat@gmail.com>
2023-02-12 01:00:38 +0700

    db

---
+++ msaa.sql
@@ -0,0 +1,209 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 11 Feb 2023 pada 18.59
+-- Versi server: 10.4.24-MariaDB
+-- Versi PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `msaa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `lp`
+--
+
+CREATE TABLE `lp` (
+  `id` int(11) NOT NULL,
+  `comp_name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telp` varchar(15) NOT NULL,
+  `addres` varchar(255) NOT NULL,
+  `logo` varchar(255) NOT NULL,
+  `visi` longtext NOT NULL,
+  `misi` longtext NOT NULL,
+  `about` varchar(500) NOT NULL,
+  `facebook` varchar(255) NOT NULL,
+  `instagram` varchar(255) NOT NULL,
+  `whatsapp` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `lp`
+--
+
+INSERT INTO `lp` (`id`, `comp_name`, `email`, `telp`, `addres`, `logo`, `visi`, `misi`, `about`, `facebook`, `instagram`, `whatsapp`) VALUES
+(1, 'MSAA', 'msaa@gmail.com', '021123456', 'jakarta', '', '', '', '', '', '', '0812123456'),
+(2, '', '', '', '', 'LogoMSAA.png', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `portfolio`
+--
+
+CREATE TABLE `portfolio` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `ficture` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `service`
+--
+
+CREATE TABLE `service` (
+  `id` int(11) NOT NULL,
+  `service_1` longtext DEFAULT NULL,
+  `service_2` longtext DEFAULT NULL,
+  `service_3` longtext DEFAULT NULL,
+  `service_4` longtext DEFAULT NULL,
+  `service_5` longtext DEFAULT NULL,
+  `service_6` longtext DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `socialmedia`
+--
+
+CREATE TABLE `socialmedia` (
+  `id` int(11) NOT NULL,
+  `facebook` varchar(500) NOT NULL,
+  `instagram` varchar(500) NOT NULL,
+  `tweeter` varchar(500) NOT NULL,
+  `youtube` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `team`
+--
+
+CREATE TABLE `team` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `positions` varchar(255) NOT NULL,
+  `quote` varchar(255) NOT NULL,
+  `img` varchar(255) NOT NULL,
+  `socialmeida_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(8) NOT NULL,
+  `level` varchar(1) NOT NULL,
+  `img` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `lp`
+--
+ALTER TABLE `lp`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `portfolio`
+--
+ALTER TABLE `portfolio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `service`
+--
+ALTER TABLE `service`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `socialmedia`
+--
+ALTER TABLE `socialmedia`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `team`
+--
+ALTER TABLE `team`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `lp`
+--
+ALTER TABLE `lp`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT untuk tabel `portfolio`
+--
+ALTER TABLE `portfolio`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `service`
+--
+ALTER TABLE `service`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `socialmedia`
+--
+ALTER TABLE `socialmedia`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `team`
+--
+ALTER TABLE `team`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
