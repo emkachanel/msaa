87bf256e8b6c45505d399d413ae62e611caf6104
tambat <tambat.sat@gmail.com>
2023-02-12 00:36:51 +0700

    update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,20 @@
 <?php
 
 namespace App\Controllers;
-
+use App\Models\lpModel;
 class Home extends BaseController
 {
+    protected $lpModel;
+    public function __construct()
+    {
+        $this->lpModel = new lpModel();
+    }
     public function index()
     {
-        return view('welcome_message');
+        $profil = $this->lpModel->getprofil();
+        $data = [
+            'profil'=>$profil,
+        ];
+        return view('landpage',$data);
     }
 }
---
+++ app/Models/lpModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class lpModel extends Model
+{
+    protected $table      = 'lp';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'id',
+        'comp_name',
+        'visi',
+        'misi',
+        'about',
+        'addres',
+        'telp',
+        'email',
+        'logo',
+        'facebook',
+        'whatsapp',
+        'instagram'
+    ];
+
+    public function getprofil($id=false)
+    {
+        if($id==false){
+            return $this->findAll();
+        }return $this->where(['id'=>$id])->first();
+            
+       
+    }
+    
+   
+
+}
